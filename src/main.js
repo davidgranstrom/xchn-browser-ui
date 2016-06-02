59510eb3edcebb9b408c4cd98a0d3feb865bfc51
David Granstrom <david@davidgranstrom.com>
2016-06-02 18:28:39 +0200

    Don't create global vars

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
-var nx = window.nx;
+// import ChannelComponent from 'ui/channelComponent'
+import Slider from './features/components/slider';
+
+const nx = window.nx;
+nx.globalWidgets = false; // don't create global variables
 
 nx.onload = () => {
   console.log('loaded nexusui');
   // setup nexusui callbacks here
 };
 
-var $multislider = $('<canvas>').attr('nx', 'multislider');
-$('#container').append($multislider);
